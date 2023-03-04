c1f8588a362b8768cbcbd286160cf6b99f362618
Lucas-Lion <lucaslion350@gmail.com>
2023-03-04 00:22:06 -0300

    modificando cod

--- nana/paginas/anamnese/js/script.js
+++ nana/paginas/anamnese/js/script.js
@@ -1,83 +1,31 @@
-    const opcaoSim = document.getElementById('opcao-sim')
-    const resposta = document.getElementById('resposta-texto')
-
-    // RUN 0
-        
+function setRequiredStatus(input, required) {
+    if (required) {
+      input.setAttribute('required', true);
+    } else {
+      input.removeAttribute('required');
+    }
+  }
+  
+  function attachChangeListeners(opcaoSim, opcaoNao, resposta) {
     opcaoSim.addEventListener('change', function() {
-        if (opcaoSim.checked) {
-        resposta.required = true
-        } else {
-        resposta.removeAttribute('required')
-        }
-    })
-
-    const opcaoNao = document.getElementById('opcao-nao')
-              
+      setRequiredStatus(resposta, opcaoSim.checked);
+    });
     opcaoNao.addEventListener('change', function() {
-        if (opcaoNao.checked) {
-        resposta.removeAttribute('required')
-        }
-    })
-
-    const opcaoSim1 = document.getElementById('opcao-sim1')
-    const resposta1 = document.getElementById('resposta-texto1')
-
-    // RUN 1
-
-    opcaoSim1.addEventListener('change', function() {
-        if (opcaoSim1.checked) {
-        resposta1.required = true
-        } else {
-        resposta1.removeAttribute('required')
-        }
-    })
-
-    const opcaoNao1 = document.getElementById('opcao-nao1')
-              
-    opcaoNao1.addEventListener('change', function() {
-        if (opcaoNao1.checked) {
-        resposta1.removeAttribute('required')
-        }
-    })
-
-    // RUN 2
-
-    const opcaoSim2 = document.getElementById('opcao-sim2')
-    const resposta2 = document.getElementById('resposta-texto2')
-
-    opcaoSim2.addEventListener('change', function() {
-        if (opcaoSim2.checked) {
-        resposta2.required = true
-        } else {
-        resposta2.removeAttribute('required')
-        }
-    })
-
-    const opcaoNao2 = document.getElementById('opcao-nao2')
-              
-    opcaoNao2.addEventListener('change', function() {
-        if (opcaoNao2.checked) {
-        resposta2.removeAttribute('required')
-        }
-    })
-
-    // RUN 3
-
-    const opcaoSim3 = document.getElementById('opcao-sim3')
-    const resposta3 = document.getElementById('resposta-texto3')
-
-    opcaoSim3.addEventListener('change', function() {
-        if (opcaoSim3.checked) {
-        resposta3.required = true
-        } else {
-        resposta3.removeAttribute('required')
-        }
-    })
-
-    const opcaoNao3 = document.getElementById('opcao-nao3')
-              
-    opcaoNao3.addEventListener('change', function() {
-        if (opcaoNao3.checked) {
-        resposta3.removeAttribute('required')
-        }
-    })
+      setRequiredStatus(resposta, false);
+    });
+  }
+  
+  const perguntas = [
+    {simId: 'opcao-sim', naoId: 'opcao-nao', respostaId: 'resposta-texto'},
+    {simId: 'opcao-sim1', naoId: 'opcao-nao1', respostaId: 'resposta-texto1'},
+    {simId: 'opcao-sim2', naoId: 'opcao-nao2', respostaId: 'resposta-texto2'},
+    {simId: 'opcao-sim3', naoId: 'opcao-nao3', respostaId: 'resposta-texto3'},
+  ];
+  
+  perguntas.forEach((pergunta) => {
+    const opcaoSim = document.getElementById(pergunta.simId);
+    const opcaoNao = document.getElementById(pergunta.naoId);
+    const resposta = document.getElementById(pergunta.respostaId);
+    attachChangeListeners(opcaoSim, opcaoNao, resposta);
+  });
+  
