6b941a08735bc26e4263685d0b87bfb350bdc5f6
Lucas-Lion <lucaslion350@gmail.com>
2023-03-25 23:00:18 -0300

    arrumando código

--- busca/js/search.js
+++ busca/js/search.js
@@ -1,15 +1,23 @@
-// Pesquisa de Ramais
-function search_ramal() {
+function search_name() {
   let input = document.getElementById("ipesquisa").value;
- input = input.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "");
-  console.log(input)
-  let x = document.getElementsByClassName("dados");
+  input = input.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+  console.log(input);
+  
+  // Recupera os dados do localStorage
+  let pessoas = JSON.parse(localStorage.getItem('pessoas') || '[]');
 
-  for (i = 0; i < x.length; i++) {
-    if (!x[i].innerHTML.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "").includes(input)) {
-      x[i].style.display = "none";
+  for (let i = 0; i < pessoas.length; i++) {
+    let pessoa = pessoas[i];
+    let nome = pessoa.nome.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+    let setor = pessoa.setor.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+    let email = pessoa.email.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+
+    if (!nome.includes(input) && !setor.includes(input) && !email.includes(input)) {
+      // Oculta a linha correspondente na tabela
+      document.getElementById("linha_" + i).style.display = "none";
     } else {
-      x[i].style.display = "table-row";
+      // Exibe a linha correspondente na tabela
+      document.getElementById("linha_" + i).style.display = "table-row";
     }
   }
 }
