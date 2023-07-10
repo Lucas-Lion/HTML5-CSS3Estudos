b340be614f4954a17d0e4d0373b1de99c888d39b
Lucas Lion <lucaslion350@gmail.com>
2023-07-10 16:45:39 -0300

    teste sistema

---
+++ sistema-teste/scripts/logout.js
@@ -0,0 +1,6 @@
+var logoutButton = document.getElementById('logoutButton');
+
+logoutButton.addEventListener('click', function() {
+  // Redireciona o usuário para a página login.php
+  window.location.href = '../index.php';
+});
