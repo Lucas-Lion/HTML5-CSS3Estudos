7faaa3e5538d3cd14607d55975560254608b2021
Lucas-Lion <101745765+Lucas-Lion@users.noreply.github.com>
2023-11-23 20:46:39 -0300

    Organizando projetos

--- sistema-teste/scripts/logout.js
+++
@@ -1,6 +0,0 @@
-var logoutButton = document.getElementById('logoutButton');
-
-logoutButton.addEventListener('click', function() {
-  // Redireciona o usuário para a página login.php
-  window.location.href = '../index.php';
-});
